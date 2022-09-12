e115b24762e241aad982b027c168271487b1e758
Vakulenko <og4youforever@gmail.com>
2022-09-12 20:31:09 +0300

    Done homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Author author = new Author("Pyshkin Aleksandr Sergeevich");
+     Book poltava = new Book(author, 1828, "Poltava");
+        poltava.getPublisherYear(1828);
+        System.out.println(poltava);
+
+     Author block = new Author("Block Aleksandr Aleksandrovich");
+     Book ramzes = new Book(author, 1921, "Ramzes");
+        ramzes.getPublisherYear(1921);
+        System.out.println(ramzes);
     }
 }
